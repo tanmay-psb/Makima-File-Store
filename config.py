78709225043de2824d625a79d2cc349477aa56f1
X A I D E N <93833994+EternalChronos@users.noreply.github.com>
2024-03-26 21:43:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5262929129"))
 PORT = os.environ.get("PORT", "8030")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://managementcosmosx:the_COSMOSX1@cluster00.mfqojxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster00")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://cosmosxd:cosmosxd@cluster0.5jdkxqt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001698824227"))
