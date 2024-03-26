f3aaa9bb5884735d7ea37941fdbbcc1cc243a317
X A I D E N <93833994+EternalChronos@users.noreply.github.com>
2024-03-26 18:38:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5262929129"))
 PORT = os.environ.get("PORT", "8030")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://managementcosmosx:the_COSMOSX1@cluster00.mfqojxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster00")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster00")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001698824227"))
@@ -65,7 +65,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(5191566338)
+ADMINS.append(5262929129)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
